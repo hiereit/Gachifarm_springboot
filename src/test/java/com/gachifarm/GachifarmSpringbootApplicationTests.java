8073a823d071f6fd93c8f3c3cb62e2892f3986fc
hiereit <68723848+hiereit@users.noreply.github.com>
2021-05-25 20:16:20 +0900

    develop spring-setting push

---
+++ src/test/java/com/gachifarm/GachifarmSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gachifarm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GachifarmSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
