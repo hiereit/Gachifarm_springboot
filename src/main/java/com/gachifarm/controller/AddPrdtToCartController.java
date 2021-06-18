86c4d762398c205d00066c0939cadc747bd08399
yyj424 <69260313+yyj424@users.noreply.github.com>
2021-06-19 02:40:06 +0900

    commit

--- src/main/java/com/gachifarm/controller/AddPrdtToCartController.java
+++ src/main/java/com/gachifarm/controller/AddPrdtToCartController.java
@@ -7,20 +7,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.SessionAttributes;
 
-import com.gachifarm.dao.ProductDao;
 import com.gachifarm.domain.CartPK;
 import com.gachifarm.domain.CartProduct;
-import com.gachifarm.domain.Product;
 import com.gachifarm.repository.CartRepository;
 @Controller
 @SessionAttributes("userSession")
 public class AddPrdtToCartController {
 	@Autowired
 	private CartRepository cartRepository;
-	@Autowired
-	private ProductDao productDao;
-	//@Autowired
-	//private AccountRepository accountRepository;
 
 	@RequestMapping("/cart/{product_id}/add")
 	public String addCartProduct(@PathVariable("product_id") int productId, @RequestParam("quantity") int cartQuantity) throws Exception {
