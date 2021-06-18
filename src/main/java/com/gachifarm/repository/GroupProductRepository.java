47725b2840e68a69e3e8c0f29972eae2a4657a3d
a-taeyeon <taeyeon0107@naver.com>
2021-06-19 02:36:25 +0900

    groupProductRepository

--- src/main/java/com/gachifarm/repository/GroupProductRepository.java
+++ src/main/java/com/gachifarm/repository/GroupProductRepository.java
@@ -1,8 +1,11 @@
 package com.gachifarm.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gachifarm.domain.GroupProduct;
+public interface GroupProductRepository extends JpaRepository<GroupProduct, String>{
 
-public interface GroupProductRepository extends JpaRepository<GroupProduct, Integer>{
+	List<GroupProduct> findGroupProductByUserId(String userId);
 }
