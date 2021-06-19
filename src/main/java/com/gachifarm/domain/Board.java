df49fe420b990989928ce5321edea513f98d736f
winter28 <leegoeun1218@naver.com>
2021-06-19 16:32:13 +0900

    Revert "문의게시판 글 쓰기, 보기, 리스트"
    This reverts commit 41edffb55e751135e4a8042be84d1af5dfbeb0b3.

--- src/main/java/com/gachifarm/domain/Board.java
+++ src/main/java/com/gachifarm/domain/Board.java
@@ -5,50 +5,23 @@ import java.util.Date;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.SequenceGenerator;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.springframework.format.annotation.DateTimeFormat;
 
 @SuppressWarnings("serial")
 @Entity
 public class Board implements Serializable {
 	@Id
-	@SequenceGenerator(name="BOARD_SEQ_GENERATOR",
-	sequenceName="BOARD_SEQUENCE", initialValue=1, allocationSize=1)
-	@GeneratedValue(strategy=GenerationType.SEQUENCE,
-	generator="BOARD_SEQ_GENERATOR")
-	@Column(name="board_id")
-	private int boardId;
-	@Column(name="product_id", nullable = true)
-	private Integer productId;
+	@Column(name="product_id")
+	private int productId;
 	@Column(name="user_id")
 	private String userId;
 	private String question;
 	private String title;
 	private String boardPW;
-	@Temporal(TemporalType.DATE)
-	@DateTimeFormat(pattern="yyyy-MM-dd")
 	@Column(name="board_date")
 	private Date boardDate;
 	private String answer;
 	
-	@ManyToOne
-	@JoinColumn(name="product_id", insertable=false, updatable=false)
-	private Product product;
-	
-	public int getBoardId() {
-		return boardId;
-	}
-	public void setBoardId(int boardId) {
-		this.boardId = boardId;
-	}
 	public String getUserId() {
 		return userId;
 	}
@@ -73,10 +46,10 @@ public class Board implements Serializable {
 	public void setBoardPW(String boardPW) {
 		this.boardPW = boardPW;
 	}
-	public Integer getProductId() {
+	public int getProductId() {
 		return productId;
 	}
-	public void setProductId(Integer productId) {
+	public void setProductId(int productId) {
 		this.productId = productId;
 	}
 	public Date getBoardDate() {
@@ -90,12 +63,6 @@ public class Board implements Serializable {
 	}
 	public void setAnswer(String answer) {
 		this.answer = answer;
-	}
-	public Product getProduct() {
-		return product;
-	}
-	public void setProduct(Product product) {
-		this.product = product;
 	}
 	
 	
--- src/main/java/com/gachifarm/repository/BoardRepository.java
+++ src/main/java/com/gachifarm/repository/BoardRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.gachifarm.domain.Board;
 
-public interface BoardRepository extends JpaRepository<Board, Integer>{
+public interface BoardRepository extends JpaRepository<Board, String>{
 
 	
 }
