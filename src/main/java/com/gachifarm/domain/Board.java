5ba4645e3ba9721d503fd8164e28abe2621ea66d
a-taeyeon <taeyeon0107@naver.com>
2021-06-20 00:17:10 +0900

    board

--- src/main/java/com/gachifarm/domain/Board.java
+++ src/main/java/com/gachifarm/domain/Board.java
@@ -1,27 +1,56 @@
 package com.gachifarm.domain;
-
 import java.io.Serializable;
 import java.util.Date;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
-
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.SequenceGenerator;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.*;
+import org.springframework.format.annotation.DateTimeFormat;
 @SuppressWarnings("serial")
 @Entity
 public class Board implements Serializable {
 	@Id
-	@Column(name="product_id")
-	private int productId;
+	@SequenceGenerator(name="BOARD_SEQ_GENERATOR",
+	sequenceName="BOARD_SEQUENCE", initialValue=1, allocationSize=1)
+	@GeneratedValue(strategy=GenerationType.SEQUENCE,
+	generator="BOARD_SEQ_GENERATOR")
+	@Column(name="board_id")
+	private int boardId;
+	@Column(name="product_id", nullable = true)
+	private Integer productId;
 	@Column(name="user_id")
 	private String userId;
+	@Size(max=2000, message="내용은 2000Byte까지만 입력 가능합니다.")
+	@NotEmpty(message="내용은 필수로 입력해야 합니다.")
 	private String question;
+	@Size(max=50, message="내용은 50Byte까지만 입력 가능합니다.")
+	@NotEmpty(message="제목은 필수로 입력해야 합니다.")
 	private String title;
+	@Size(max=20, message="비밀번호는 20Byte까지만 입력 가능합니다.")
 	private String boardPW;
+	@Temporal(TemporalType.DATE)
+	@DateTimeFormat(pattern="yyyy-MM-dd")
 	@Column(name="board_date")
 	private Date boardDate;
 	private String answer;
 	
+	@ManyToOne
+	@JoinColumn(name="product_id", insertable=false, updatable=false)
+	private Product product;
+	
+	public int getBoardId() {
+		return boardId;
+	}
+	public void setBoardId(int boardId) {
+		this.boardId = boardId;
+	}
 	public String getUserId() {
 		return userId;
 	}
@@ -46,10 +75,10 @@ public class Board implements Serializable {
 	public void setBoardPW(String boardPW) {
 		this.boardPW = boardPW;
 	}
-	public int getProductId() {
+	public Integer getProductId() {
 		return productId;
 	}
-	public void setProductId(int productId) {
+	public void setProductId(Integer productId) {
 		this.productId = productId;
 	}
 	public Date getBoardDate() {
@@ -63,6 +92,18 @@ public class Board implements Serializable {
 	}
 	public void setAnswer(String answer) {
 		this.answer = answer;
+	}
+	public Product getProduct() {
+		return product;
+	}
+	public void setProduct(Product product) {
+		this.product = product;
+	}
+	@Override
+	public String toString() {
+		return "Board [boardId=" + boardId + ", productId=" + productId + ", userId=" + userId + ", question="
+				+ question + ", title=" + title + ", boardPW=" + boardPW + ", boardDate=" + boardDate + ", answer="
+				+ answer + ", product=" + product + "]";
 	}
 	
 	
